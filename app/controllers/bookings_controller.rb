99033cb29d11c1568e12719b5762981576966c41
ricardo canas <canasricardo17@gmail.com>
2024-02-16 15:34:19 +0100

    creation de new et create sur le booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,29 @@
 class BookingsController < ApplicationController
+  before_action :set_player, only: [:new, :create]
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.player = @player
+    @user_id = current_user.id
+    @booking.user_id = @user_id
+    if @booking.save
+      redirect_to player_path(@player)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:player_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :player_id)
   end
 end
