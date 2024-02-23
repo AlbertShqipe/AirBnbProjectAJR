33dfd4bba5c7f1331f58a9376d6103f851b14165
Julien Marchand <nifrael66@gmail.com>
2024-02-23 16:49:40 +0100

    New front 1.0 on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,6 @@ class DashboardsController < ApplicationController
   def index
     @players = Player.where(owner_id: current_user)
     @bookings = current_user.bookings
+    @today = Date.today
   end
 end
