0d9bffd93c198416a46ded6989c8b16c0388ecfa
Albert Nikolli <albert_nikolli@icloud.com>
2024-02-25 21:18:30 +0100

    Cleaned the footer.scss, Added a line to be able to understand the page position of the user on a given moment, cleaned the style of the page, still missing some work on the dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def some_action
+    @current_page = request.path
+  end
+
   protected
 
   def configure_permitted_parameters
